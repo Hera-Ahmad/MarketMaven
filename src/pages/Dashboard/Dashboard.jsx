df86503b312532379196e6a314262018e18eb284
Hera Ahmad <ahmadhera0512@gmail.com>
2024-08-01 17:23:49 +0530

    Chakara it added

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import DashboardLayout from '../../components/DashboardLayout';
 import PortfolioSection from './components/PortfolioSection';
+import PriceSection from './components/PriceSection';
+import { Grid, GridItem } from '@chakra-ui/react';
+
 const Dashboard = () => {
   return (
     <DashboardLayout title="Dashboard">
-     <PortfolioSection/>
+      <Grid
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          md: "repeat(2, 1fr)",
+        }}
+        gap={6}
+      >
+        <GridItem colSpan={2}>
+          <PortfolioSection />
+        </GridItem>
+
+
+        <GridItem colSpan={1}>
+          <PriceSection />
+        </GridItem>
+      </Grid>
     </DashboardLayout>
   );
 };
