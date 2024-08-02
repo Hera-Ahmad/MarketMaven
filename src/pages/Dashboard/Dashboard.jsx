743c5f3b8aa7b8a809ef033bce293ee373556e64
Hera Ahmad <ahmadhera0512@gmail.com>
2024-08-02 21:24:02 +0530

    Updated

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import PortfolioSection from './components/PortfolioSection';
 import PriceSection from './components/PriceSection';
 import { Grid, GridItem } from '@chakra-ui/react';
 import Transactions from './components/Transactions';
+import InfoCard from './components/InfoCard';
 
 
 const Dashboard = () => {
@@ -12,11 +13,14 @@ const Dashboard = () => {
       <Grid
         templateColumns={{
           base: "repeat(1, 1fr)",
-          md: "repeat(2, 1fr)",
+          xl: "repeat(2, 1fr)",
         }}
         gap={6}
       >
-        <GridItem colSpan={2}>
+        <GridItem colSpan={{
+          base:1,
+          xl:2,
+        }}>
           <PortfolioSection />
         </GridItem>
 
@@ -29,6 +33,23 @@ const Dashboard = () => {
           <Transactions />
         </GridItem>
 
+        <GridItem colSpan={1}>
+          <InfoCard ImageUrl="/dot_bg.svg"
+            text=" Learn more about Loans – Keep your Bitcoin, access it’s value without
+            selling it"
+              tagText="Loan"
+              inverted={false}
+          />
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <InfoCard
+            inverted={true}
+            tagText="Contact"
+            imgUrl="/grid_bg.svg"
+            text="Learn more about our real estate, mortgage, and  corporate account services"
+          />
+        </GridItem>
 
       </Grid>
     </DashboardLayout>
