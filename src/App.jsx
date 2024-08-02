f4eeca473e74bba76803dcaf52ae33424f0a9554
Hera Ahmad <ahmadhera0512@gmail.com>
2024-08-02 21:22:38 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import Dashboard from './pages/Dashboard/Dashboard';
+import * as React from "react";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Transactions from './pages/Dashboard/components/Transactions';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:      <Dashboard/>,
+    },
+    {
+      path: "/transaction",
+      element:      <Transactions/>,
+    },
+    {
+      path: "/Support",
+      element: <Support/>,
+    },
+  ]);
+
+
   return (
      <>
+     <RouterProvider router={router} />
      <Dashboard/>
     </>
   );
